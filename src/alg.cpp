892290d690e987c86effd46a2f2901e3aeacb48e
RomaKlyukin <125215143+RomaKlyukin@users.noreply.github.com>
2023-02-26 00:09:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,37 +18,37 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-	uint64_t num_Prime = 0, sc = 1;
-	do {
-		if (checkPrime(sc) == true) {
-			num_Prime += 1;
-		}
-		if (num_Prime == n) { 
-				return sc;
-			}
-	sc += 1;
-	} while (sc > 0);
-	return 0;
+  uint64_t num_Prime = 0, sc = 1;
+  do {
+    if (checkPrime(sc) == true) {
+      num_Prime += 1;
+    }
+    if (num_Prime == n) {
+      return sc;
+    }
+  sc += 1;
+  } while (sc > 0);
+  return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	uint64_t sc = value + 1;
-	do {
-		if (checkPrime(sc) == true) {
-			return sc;
-		}
-	sc += 1;
-	} while (sc > 0);
-	return 0;
+  uint64_t sc = value + 1;
+  do {
+    if (checkPrime(sc) == true) {
+      return sc;
+    }
+  sc += 1;
+  } while (sc > 0);
+  return 0;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sc = 1, sum = 0;
-	do {
-		if (checkPrime(sc) == true) {
-			sum += sc; 
-		}
-	sc += 1;
-	} while (sc < hbound);
-	return sum;
+  do {
+    if (checkPrime(sc) == true) {
+      sum += sc;
+    }
+  sc += 1;
+  } while (sc < hbound);
+  return sum;
 }
